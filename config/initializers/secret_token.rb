09fddefd101d2f9b2574f71c4275c5b8c0719388
Jan Vlnas <git@jan.vlnas.cz>
2011-08-16 11:03:55 +0200

    Init blank app wout Test::Unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omega::Application.config.secret_token = 'c71af4cf8997a4f91a7c86ee5df869b088f7ff78cc736564ad243c2acb4b371b928d135535e9127be3d2ed814f38cabbc603d2e7c52ec2ba26ef6e94ce27806d'
