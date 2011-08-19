c25276142d2d002cea1af2c52ef7964e1769bee3
Jan Vlnas <git@jan.vlnas.cz>
2011-08-19 14:17:55 +0200

    Add Role, Name to User

---
+++ db/migrate/20110819120939_add_name_and_role_to_users.rb
@@ -0,0 +1,10 @@
+class AddNameAndRoleToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    change_column :users, :name, :string, null: false #Makes a lot of fucking sense. http://strd6.com/2009/04/adding-a-non-null-column-with-no-default-value-in-a-rails-migration/
+
+    add_column :users, :role, :string, null:false, default: 'user'
+
+    add_index :users, :name, unique: true
+  end
+end
