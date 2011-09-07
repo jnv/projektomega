f2efc1d64301cea0c20b3d49931ca708d09629c2
Jan Vlnas <git@jan.vlnas.cz>
2011-09-08 01:51:48 +0200

    authentication spec for registration

--- spec/acceptance/authentication_spec.rb
+++ spec/acceptance/authentication_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'acceptance/acceptance_helper'
 
 feature 'Registration', %q{
@@ -5,7 +6,17 @@ feature 'Registration', %q{
   I want to be able to sign-up
 } do
 
-  scenario 'Signing in with correct credentials' do
+  scenario 'Register with correct credentials' do
+    visit registration
+    within("#user_new") do
+      fill_in 'Jméno', with: 'Leeroy Jenkins'
+      fill_in 'E-mail', with: 'user@example.com'
+      fill_in 'Heslo', with: 'please'
+      fill_in 'user_password_confirmation', with: 'please'
+      click_button 'Zaregistrovat se'
+    end
+
+    page.should have_content 'Registrace byla úspěšná.'
     
   end
 
