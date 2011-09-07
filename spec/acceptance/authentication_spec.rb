2848d6b762d8af2bd0243eeb182da1bc0def89ce
Jan Vlnas <git@jan.vlnas.cz>
2011-09-07 23:05:03 +0200

    Prepare acceptance specs

---
+++ spec/acceptance/authentication_spec.rb
@@ -0,0 +1,12 @@
+require 'acceptance/acceptance_helper'
+
+feature 'Registration', %q{
+  As an unregistered user
+  I want to be able to sign-up
+} do
+
+  scenario 'Signing in with correct credentials' do
+    
+  end
+
+end
