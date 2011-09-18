27df0638c7f6ba6ee0abc2635766ad7be7c8e146
Jan Vlnas <git@jan.vlnas.cz>
2011-09-18 16:41:33 +0200

    moar proper character testing

--- spec/acceptance/characters_spec.rb
+++ spec/acceptance/characters_spec.rb
@@ -46,8 +46,8 @@ feature 'Characters' do
 
       click_button 'Uložit změny'
 
-      page.should have_content("Agent byl upraven")
       should_be_on character_path(@char)
+      page.should have_content("Agent byl upraven")
 
       find(".character_details") do |char|
         char.should have_content("My New Name")
@@ -68,26 +68,79 @@ feature 'Characters' do
         char.should_not have_link "Upravit"
       end
       visit character_path(char2)
+      page.should_not have_content("Upravit agenta")
+      edit_character_path(char2)
       pending "Finalize 'access denied' message" do
         access_should_be_denied
       end
     end
+
+    scenario "should be able to upload agent's portrait" do
+      pending
+    end
+
   end
 
-  describe 'authorized user' do
+  describe 'administrator' do
 
     before :each do
       @user = Factory :admin
-      @char = Factory(:orphaned_character)
+      @char = Factory :orphaned_character
       sign_in_with @user
       visit characters_path
     end
 
     scenario "should be able to edit any character" do
-      
+
+      within(css_dom_id(@char)) do
+        click_link "Upravit"
+      end
+
+      fill_in 'Jméno', with: "My New Name"
+
+      click_button 'Uložit změny'
+
+      should_be_on character_path(@char)
+      page.should have_content("Agent byl upraven")
+
+      find(".character_details") do |char|
+        char.should have_content("My New Name")
+      end
+
+      page.should have_link("Upravit agenta")
+
     end
 
+    describe "should be able to create new character" do
+
+      scenario "with just number and name" do
+        click_link "Vytvořit agenta"
+        fill_in 'Číslo agenta', with: '958'
+        fill_in 'Jméno', with: 'Agent Gadget'
+
+        click_button "Vytvořit"
+
+        page.should have_content("Agent byl vytvořen")
+      end
+
+      scenario "but not without name or number" do
+
+        click_link "Vytvořit agenta"
+        fill_in "Číslo agenta", with: "abc"
+        click_button "Vytvořit"
+
+        page.should have_selector('p.inline-errors')
+        fill_in "Číslo agenta", with: "13"
+        click_button "Vytvořit"
+
+        page.should have_selector('p.inline-errors')
+
+        fill_in "Jméno", with: "Pinkie Pie"
+        click_button "Vytvořit"
+
+        page.should have_content("Agent byl vytvořen")
+      end
+
+    end
   end
-
-
 end
