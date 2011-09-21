025a9b9baadf5927c0541e5e4d0b4e8a8b18265c
Jan Vlnas <git@jan.vlnas.cz>
2011-09-21 22:06:14 +0200

    Airbrake notification, remove some debug dumps

--- app/controllers/authorized_controller.rb
+++ app/controllers/authorized_controller.rb
@@ -4,15 +4,14 @@ class AuthorizedController < ApplicationController
 
   #before_filter :authenticate_user!
   #enable_authorization
-  enable_authorization do |exception|
+  enable_authorization do |ex|
     redirect_url = new_session_path(:user)
     if user_signed_in?
       redirect_url = root_url
     end
-    
-    ###TODO report to hoptoad
-    exception.default_message = t('unauthorized.default_message')
-    redirect_to redirect_url, :alert => exception.message
+    ex.default_message = t('unauthorized.default_message')
+    notify_airbrake(ex)
+    redirect_to redirect_url, :alert => ex.message
   end
 
   load_and_authorize_resource
