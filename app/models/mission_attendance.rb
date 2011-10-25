3f5d9508e40bf0f3fdb53a673c3f14284e2e0f97
Jan Vlnas <git@jan.vlnas.cz>
2011-10-26 01:30:54 +0200

    MissionAttendance: Add with_evaluations scope

--- app/models/mission_attendance.rb
+++ app/models/mission_attendance.rb
@@ -4,9 +4,15 @@ class MissionAttendance < ActiveRecord::Base
   
   belongs_to :mission
   belongs_to :character
-  has_many :evaluations, class_name: 'Evaluation', foreign_key: [:mission_id, :character_id]
+  has_one :evaluation, foreign_key: [:mission_id, :character_id]
+
+  scope :with_evaluations, includes(:evaluation)
+  #scope :evaluations_nil, evaluations_all.where(evaluations: {content: nil})
 
   delegate :user, :to => :character, :prefix => true, :allow_nil => true
 
+  def self.evaluations
+
+  end
 
 end
--- spec/models/mission_attendance_spec.rb
+++ spec/models/mission_attendance_spec.rb
@@ -1,17 +1,31 @@
 require 'spec_helper'
 
 describe MissionAttendance do
+  it "should have multiple primary ids" do
+    MissionAttendance.primary_key.should eq([:mission_id, :character_id])
+  end
 
   describe "associations" do
     it { should belong_to :mission }
     it { should belong_to :character }
-    it "has many evaluations" do
-      MissionAttendance.reflect_on_association(:evaluations).macro.should == :has_many #XXX Shoulda doesn't like CPK
+    it "has one evaluation" do
+      MissionAttendance.reflect_on_association(:evaluation).macro.should == :has_one #XXX Shoulda doesn't like CPK
     end
   end
 
-  it "should have multiple primary ids" do
-    MissionAttendance.primary_key.should eq([:mission_id, :character_id])
+
+  describe "scopes" do
+    before do
+      @mission = Factory(:attended_mission, attendees: 4)
+      @evaluation = Factory(:evaluation, attended_mission: @mission)
+    end
+
+    specify { MissionAttendance.joins(:evaluation).count.should == 1 }
+
+    describe "#evaluations_all" do
+      subject { MissionAttendance.with_evaluations }
+      its(:count) { should == @mission.attendances.count}
+    end
   end
 
   describe "#character_user" do
