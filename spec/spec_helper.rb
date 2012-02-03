4c95422cafae2e3fc72dfc65816f57549c7ef636
Jan Vlnas <git@jan.vlnas.cz>
2012-02-03 01:44:57 +0100

    Clean-up Carrierwave test files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,6 +29,13 @@ Spork.prefork do
     # examples within a transaction, remove the following line or assign false
     # instead of true.
     config.use_transactional_fixtures = true
+
+    #CarrierWave
+    config.after(:all) do
+      if Rails.env.test?
+        FileUtils.rm_rf(Dir["#{Rails.root}/spec/support/uploads"])
+      end
+    end
   end
 
   ActiveSupport::Dependencies.clear
@@ -44,4 +51,17 @@ Spork.each_run do
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
   ActiveSupport::Dependencies.clear
+
+  CarrierWave::Uploader::Base.descendants.each do |klass|
+    next if klass.anonymous?
+    klass.class_eval do
+      def cache_dir
+        "#{Rails.root}/spec/support/uploads/tmp"
+      end
+
+      def store_dir
+        "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
+    end
+  end
 end
