22841bae4d29e556e0411bffcaee19a74355518d
Jan Vlnas <git@jan.vlnas.cz>
2012-02-04 01:07:13 +0100

    Post model spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe Post do
 
+  describe "associations" do
+    it { should belong_to(:character) }
+    it { should belong_to(:user) }
+  end
 
+  describe "validations" do
+
+      it { should validate_presence_of(:content) }
+
+    end
 
 end
