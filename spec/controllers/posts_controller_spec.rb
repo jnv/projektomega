927721d76147db295960749af9af66d6980f9565
Jan Vlnas <git@jan.vlnas.cz>
2012-02-05 20:48:49 +0100

    Prep PostsController spec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,13 +2,8 @@ require 'spec_helper'
 
 describe PostsController do
 
-  # This should return the minimal set of attributes required to create a valid
-  # Post. As you add validations to Post, be sure to
-  # update the return value of this method accordingly.
-  def valid_attributes
-    {}
-  end
-  
+  let(:valid_attributes) { Factory.attributes_for(:mission) }
+
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # PostsController. Be sure to keep this updated too.
