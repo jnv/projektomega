ab6a620819e4bc531cba7a7478075d4cc5bb32cb
Jan Vlnas <git@jan.vlnas.cz>
2012-06-03 20:50:01 +0200

    mass_assignment_sanitizer and auto_explain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,11 @@ Omega::Application.configure do
   config.assets.compress = false
   
   config.assets.digest = false
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
