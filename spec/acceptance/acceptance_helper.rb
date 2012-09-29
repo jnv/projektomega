0dade3986a75cba43dc765bec94e705f41aa18fb
Jan Vlnas <git@jan.vlnas.cz>
2012-09-29 12:35:15 +0200

    Use database cleaner for acceptance specs

--- spec/acceptance/acceptance_helper.rb
+++ spec/acceptance/acceptance_helper.rb
@@ -2,3 +2,20 @@ require 'spec_helper'
 
 # Put your acceptance spec helpers inside spec/acceptance/support
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
