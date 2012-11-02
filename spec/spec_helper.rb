49c03db6c01893077a453c89732238d81d41ec5a
Jan Vlnas <git@jan.vlnas.cz>
2012-11-02 23:19:51 +0100

    Enforce test in RAILS_ENV for engines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'spork'
 #require 'spork/ext/ruby-debug'
 
 Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
+  ENV["RAILS_ENV"] = 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
   require 'rspec/autorun'
