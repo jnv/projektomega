cf3f0809b03f297b06e98129d35cd1fe035dda79
Jan Vlnas <git@jan.vlnas.cz>
2012-12-27 19:26:32 +0100

    Capistrano: recipe for YamlDb [ci skip]

---
+++ lib/recipes/dump.rb
@@ -0,0 +1,13 @@
+namespace :dump do
+  desc "Dump data using yaml_db and copy file into db/data.yml"
+  task :db do
+    rake = fetch(:rake, 'rake')
+    rails_env = fetch(:rails_env, 'production')
+
+    remotefile = "#{current_path}/db/data.yml"
+    localfile = "db/data.yml"
+    run "cd '#{current_path}' && #{rake} db:data:dump RAILS_ENV=#{rails_env}"
+    download remotefile, localfile, via: :scp
+    run "rm #{remotefile}"
+  end
+end
